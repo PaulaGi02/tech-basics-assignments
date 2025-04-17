010f793265441ab7df3cf773b6182f72cbdcf26c
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-04-17 10:58:47 +0200

    first input

--- assignments/week2/game.py
+++ assignments/week2/game.py
@@ -1,8 +1,16 @@
+import time
+
 #start game "your potato destiny"
 #diet preference
+print("Welcome to your potato destiny. Today we want to find the perfect patato dish for \033[1myou\033[0m,\nbecause potato day is every day!")
+time.sleep(3)
+print("So lets start with your preferenced diet")
+
+diet = input("\n would you like the dish to be vegetarian or vegan?").lower()
+
 #potato role
 #potato form
 #cooking method
-#falvour vibe
+#flavour vibe
 #time effort
 #end recipe
