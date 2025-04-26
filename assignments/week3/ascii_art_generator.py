4e1c01d837cae5b92cd29adc1e257f679e8ae1e3
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-04-26 13:55:25 +0200

    addind if statements

--- assignments/week3/ascii_art_generator.py
+++ assignments/week3/ascii_art_generator.py
@@ -1,16 +1,30 @@
 import random
 
 user_input = int(input("Enter a number between 1 and 5: "))
+mood = input("please enter a mood: choose between shocked, happy or dance")
 
 shocked = [r"＜('0')＞¯", r"૮(°□°'˶)ა", r"( ˶°ㅁ°)", r"(⚆ᗝ⚆)", r"(⊙_⊙)"]
 happy = [r"(๑ˇεˇ๑)", r"(◕‿◕✿)", r"/(≧∇≦)/", r"(●＾o＾●)", r"(＾▽＾)", r"( ˶ˆᗜˆ˵ )"]
 dance = [r"ᕕ( ᐛ )ᕗ", r"¯\_(ツ)_/¯", r"(ง ˙˘˙ )ว", r"♪┏(・o･)┛♪", r"( ノ・・)ノ", r"(~‾⌣‾)~"]
 
-three_random_shocked = random.sample(shocked, user_input)
-three_random_happy = random.sample(happy, user_input)
-three_random_dance = random.sample(dance, user_input)
+random_shocked = random.sample(shocked, user_input)
+random_happy = random.sample(happy, user_input)
+random_dance = random.sample(dance, user_input)
 
-for _ in range(5):
-    for emoji in three_random_shocked:
-        print(emoji, end=' ')
-    print()
+if mood == "shocked":
+    for _ in range(5):
+        for emoji in random_shocked:
+            print(emoji, end=' ')
+        print()
+elif mood == "happy":
+    for _ in range(5):
+        for emoji in random_happy:
+            print(emoji, end=' ')
+        print()
+elif mood == "dance":
+    for _ in range(5):
+        for emoji in random_dance:
+            print(emoji, end=' ')
+        print()
+else:
+    print("Sorry, you typed in something wrong")
