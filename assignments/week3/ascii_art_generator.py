0d8f8627cb0ca56a0821b7da281e478abc15f01d
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-04-26 13:43:08 +0200

    adding emojis and random

--- assignments/week3/ascii_art_generator.py
+++ assignments/week3/ascii_art_generator.py
@@ -1,10 +1,15 @@
 import random
 
 
-my_list = [r"¯\_(ツ)_/¯", r"=^_^=", r"(ಥ﹏ಥ)"]
-random.shuffle(my_list)
+shocked = [r"＜('0')＞¯", r"૮(°□°'˶)ა", r"( ˶°ㅁ°)", r"(⚆ᗝ⚆)", r"(⊙_⊙)"]
+happy = [r"(๑ˇεˇ๑)", r"(◕‿◕✿)", r"/(≧∇≦)/", r"(●＾o＾●)", r"(＾▽＾)", r"( ˶ˆᗜˆ˵ )"]
+dance = [r"ᕕ( ᐛ )ᕗ", r"¯\_(ツ)_/¯", r"(ง ˙˘˙ )ว", r"♪┏(・o･)┛♪", r"( ノ・・)ノ", r"(~‾⌣‾)~"]
+
+three_random_shocked = random.sample(shocked, 3)
+three_random_happy = random.sample(happy, 3)
+three_random_dance = random.sample(dance, 3)
 
 for _ in range(5):
-    for emoji in my_list:
+    for emoji in three_random_shocked:
         print(emoji, end=' ')
     print()
