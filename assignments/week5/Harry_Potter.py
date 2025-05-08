2f7ea5dbd77cacb1d111cfd492285d7272a8a357
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-05-08 11:06:16 +0200

    items display

--- assignments/week5/Harry_Potter.py
+++ assignments/week5/Harry_Potter.py
@@ -1,37 +1,33 @@
-print("We are in the Gryffindor dorm. Despite of Harry's first day of school he overslept. \n "
-      "Ron is freaking out, just imagining McGonagall's reaction on their delay. \n"
-      "You need to help them ")
 
+inventory = []
 
-user_items = []
-#dictonary with rooms
-room_riddles_solved = {
-    "Great Hall": False,
-    "Forbidden Corridor": False,
-    "Moaning Myrtle' Bathroom": False,
-}
+#Items available in Gryffindor Common Room
+items_in_room = [{"name": "Wand", "type": "tool", "description": "Used to cast spells."},
+    {"name": "Marauder's Map", "type": "tool", "description": "Reveals hidden rooms."},
+    {"name": "Chocolate Frog", "type": "food", "description": "Restores energy."},
+    {"name": "Broken Quill", "type": "junk", "description": "Doesn't work anymore."},
+    {"name": "Invisibility Cloak", "type": "tool", "description": "Makes you invisible to Peeves."},
+    {"name": "Pumpkin Juice", "type": "food", "description": "A tasty Hogwarts drink."}
+]
+MAX_INVENTORY_SIZE = 5
 
-#setting room requirements
-room_requirements = {
-    "Great Hall" : {},
-    "Forbidden Corridor": {"required_item": "Wand", "riddle_required": True},
-    "Moaning Myrtle' Bathroom": {},
-}
+#displays all items you are carrying
+def show_inventory():
+    if len(inventory) == 0:
+        print ("inventory is empty")
+    else:
+        print("You are carrying: ")
+        for items in inventory:
+            print(f"- {items['name']}")
 
-#Structure
-if "Wand" in user_items:
-    print ("Congratulations! You have entered the Forbidden Corridor. Footsteps appear in the distance\n"
-           "It'll be Flinch. Let's see if you can find something useful to hide before he is coming")
-
-user_input = 0
-turns = 0
-
-def command(user_input):
-    global turns
-    turns += 1
-while turns < 10:
-    user_input = input("What do you do? ")
-    command(user_input)
+#displays all items currently in the room
+def show_room_items():
+    if len(inventory) == 0:
+        print ("the room is empty")
+    else:
+        print("You are carrying: ")
+        for items in items_in_room:
+            print(f"- {items['name']}: {items['description']}")
 
 
 
