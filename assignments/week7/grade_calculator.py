aedd09087c769d0b5220cd60115b26449e7a0e83
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-05-23 11:14:12 +0200

    adding random score

--- assignments/week7/grade_calculator.py
+++ assignments/week7/grade_calculator.py
@@ -1,4 +1,5 @@
 import csv
+import random
 
 file = "Technical Basics I_2025 - Sheet1.csv"
 
@@ -6,7 +7,13 @@ try:
     with open(file, newline = '') as csvfile:
         reader = csv.DictReader(csvfile)
         students = list(reader)
-        print(f"Loaded {len(students)} students from {file}")
+        #print(f"Loaded {len(students)} students from {file}")
 except FileNotFoundError:
     print(f"File {file} not found.")
 
+weeks = [f"Week {i}" for i in range(1, 14) if i != 6]
+
+for student in students:
+    for week in weeks:
+        if student.get(week) in (None, "", " "):
+            student[week] = str(random.randint(1, 3))
