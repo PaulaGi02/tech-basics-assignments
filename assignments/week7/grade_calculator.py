d6047fb605e135b9511255f8d9347ad907dad37d
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-05-23 23:35:57 +0200

    adding template

--- assignments/week7/grade_calculator.py
+++ assignments/week7/grade_calculator.py
@@ -1,19 +1,66 @@
+from sys import argv
 import csv
 import random
 
-file = "Technical Basics I_2025 - Sheet1.csv"
+filename = "Technical Basics I_2025 - Sheet1.csv"
+students = []
+# Step 1
+def read_csv(filename):
+    global students
+    try:
+        with open(filename, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            students = list(reader)
+            print(f"✅ Loaded {len(students)} students from '{filename}'")
+    except FileNotFoundError:
+        print ("File not found")
+        exit()
 
-try:
-    with open(file, newline = '') as csvfile:
-        reader = csv.DictReader(csvfile)
-        students = list(reader)
-        #print(f"Loaded {len(students)} students from {file}")
-except FileNotFoundError:
-    print(f"File {file} not found.")
 
-weeks = [f"Week {i}" for i in range(1, 14) if i != 6]
+# Step 2
+def populate_scores():
+    pass
 
-for student in students:
-    for week in weeks:
-        if student.get(week) in (None, "", " "):
-            student[week] = str(random.randint(1, 3))
+# Step 3
+def calculate_all():
+    # loop through all the students and calculate grades
+    pass
+
+def calculate_total(scores):
+    total = 0
+    return total
+
+def calculate_average(scores):
+    average =  0
+    return average
+
+# After the update let's save the data as a new csv file
+
+def write_csv(filename):
+    pass
+
+# Bonus
+
+def print_analysis():
+    # print average scores for stream A, B and every week
+    pass
+
+if __name__ == "__main__":
+    script, filename = argv
+
+    print("Open file:", filename)
+
+    read_csv(filename)
+
+    populate_scores()
+    calculate_all()
+
+    user_name = "[your_name]"
+
+    newname = filename.split(".")[0] + "_calculated_by_" + user_name + ".csv"
+    write_csv(newname)
+    print("New file written:", newname)
+
+    print_analysis()
+
+# Run the file with `python grade_calculator.py sheet.csv`
