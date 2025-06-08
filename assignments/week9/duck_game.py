65319ba8923a782ea83e9c8d4cee97223ed75e4a
PaulaGi02 <paula.giensch@stud.leuphana.de>
2025-06-08 13:20:08 +0200

    classes

--- assignments/week9/duck_game.py
+++ assignments/week9/duck_game.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random
+from pygame.math import Vector2
 
 pygame.init()
 
@@ -24,7 +25,62 @@ clock = pygame.time.Clock()
 duck_img = pygame.image.load("duck.png").convert_alpha()
 duck_img = pygame.transform.scale(duck_img, (CELL_SIZE, CELL_SIZE))
 # Entity Class
+class Entity:
+    def __init__(self, position):
+        self._position = Vector2(position)
+
+    @property
+    def pos(self) -> Vector2:
+        return self._position
+
+    @pos.setter
+    def pos(self, pos: Vector2) -> None:
+        self._position = Vector2(pos)
 #subclass
+class Duckling(Entity):
+    def __init__(self, snake_body):
+        super().__init__(self._generate_random_pos(snake_body))
+
+    def draw(self):
+        rect = pygame.Rect(
+            OFFSET + self._position.x * CELL_SIZE,
+            OFFSET + self._position.y * CELL_SIZE,
+            CELL_SIZE, CELL_SIZE)
+        screen.blit(duck_img, rect)
+
+    @staticmethod
+    def _generate_random_cell():
+        return Vector2(random.randint(0, NUM_CELLS - 1),
+                       random.randint(0, NUM_CELLS - 1))
+
+    def _generate_random_pos(self, snake_body):
+        pos = self._generate_random_cell()
+        while pos in snake_body:
+            pos = self._generate_random_cell()
+        return pos
+
+    def reset_position(self, snake_body):
+        self.pos = self._generate_random_pos(snake_body)
 #Game class
+class Game:
+    pass
+
 #Game loop
-#Drawing
+game = Game()
+SNAKE_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SNAKE_UPDATE, 170)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #Drawing
+    screen.fill(GREEN)
+    pygame.draw.rect(screen, DARK_GREEN,
+                        (OFFSET - 5, OFFSET - 5,
+                        CELL_SIZE * NUM_CELLS + 10,
+                        CELL_SIZE * NUM_CELLS + 10), 6)
+
+    pygame.display.update()
+    clock.tick(60)
